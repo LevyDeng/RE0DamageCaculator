3bf06ef490e883e05ce06ca634cf5964cd441528
ptrees <ptrees@126.com>
2020-07-17 23:12:14 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,40 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
+var globalData = {
+  num: 0,
+  characterData: {
+    "基础攻击": 0,
+    "暴击几率": 0,
+    "连击几率": 0,
+    "暴击伤害": 0,
+    "连击伤害": 0,
+    "伤害加成": 0,
+    "防御忽视": 0,
+    "必杀技伤害": 0
+  },
+  xinzhiqis: {
+    "0": {
+      name: "成长与归途",
+      id: 0,
+      attack: 0,
+      checked: false,
+      properties: {
+        "基础攻击": 0.1,
+        "暴击几率": 0.09,
+        "连击几率": 0.09,
+        "必杀技伤害": 0.18
+      }
+    }
+  }
+}
+
 new Vue({
+  data: globalData,
   render: h => h(App),
 }).$mount('#app')
