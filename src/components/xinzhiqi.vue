30ba4e3eee8a41ba93e78174495b8e041f3e3b97
ptrees <ptrees@126.com>
2020-07-18 22:23:39 +0800

    添加报错信息

--- src/components/xinzhiqi.vue
+++ src/components/xinzhiqi.vue
@@ -3,23 +3,27 @@
     <table border="">
       <tr>
         <td><el-checkbox border :checked="xinzhiqiData.checked"></el-checkbox></td>
-        <td v-if="xinzhiqiData.id!=-1"><el-button @click="save">保存</el-button></td>
-        <td v-if="xinzhiqiData.id==-1"><el-button>新建</el-button></td>
+        <td v-if="xinzhiqiData.id!=-1"><el-button @click="save">保存</el-button><span v-if="saveSuccess==false" style="color:red;font-size:12px">保存失败</span></td>
+        <td v-if="xinzhiqiData.id==-1"><el-button @click="save">新建</el-button></td>
       </tr>
       <tr>
         <td>名称</td>
-        <td><input :value="xinzhiqiData.name" style="font-weight:bold;font-size:14px"></td>
+        <td><input :value="xinzhiqiData.name" ref="name" style="font-weight:bold;font-size:14px"></td>
       </tr>
       <tr>
         <td>攻击力:</td>
-        <td><input :value="xinzhiqiData.attack"></td>
+        <td><input :value="xinzhiqiData.attack" ref="attack">
+          <div v-if="attackValid==false"><span style="color:red;font-size:12px">{{errorMsg}}</span></div>
+        </td>
       </tr>
       <tr v-for="(v,k,i) in xinzhiqiData.properties" :key="k">
         <td><select :ref="'select'+i">
             <option v-for="(v0,k0) in x_properties" :key="k0" :selected="k==k0 ? true : false">{{k0}}</option>
           </select></td>
-        <td><input :value="v" :ref="'input'+i"></td>
+        <td><input :value="v" :ref="'input'+i">
+        <div v-if="inputErrors[i]==true"><span style="color:red;font-size:12px">{{errorMsg}}</span></div></td>
       </tr>
+      <tr><el-button>删除</el-button></tr>
     </table>
   </div>
 </template>
@@ -37,6 +41,18 @@ export default {
     save: function() {
       var ALLVALID = true
       var p = {}
+      //初始化错误信息
+      this.attackValid=true
+        if (isNaN(this.$refs.attack.value)) {
+          this.attackValid=false
+          ALLVALID=false
+        }
+
+      this.inputErrors=[]
+      for (var i0=0;i0<this.propertiesNum;i0++) {
+        this.inputErrors.push(false)
+      }
+
       for (var i=0; i<this.propertiesNum; i++) {
         var label = this.$refs['select'+i.toString()][0].value
         var value = this.$refs["input"+i][0].value
@@ -49,10 +65,24 @@ export default {
         }
       }
       if (ALLVALID == true) {
-        this.xinzhiqiData.properties=p
-        this.saveError=false
+        if (this.xinzhiqiData.id!=-1) {
+          this.xinzhiqiData.name = this.$refs.name.value
+          this.xinzhiqiData.attack=this.$refs.attack.value
+          this.xinzhiqiData.properties=p
+          this.saveSuccess=true
+        } else {
+          var newid = this.$root.$data.num+1
+          this.$root.$data.xinzhiqis[newid]={
+            id: newid,
+            name: this.$refs.name.value,
+            attack: this.$refs.attack.value,
+            checked: false,
+            properties: p
+          }
+          this.$root.$data.num += 1
+        }
       } else {
-        this.saveError = true
+          this.saveSuccess = false
       }
     },
     isValid: function(label,input) {
@@ -89,8 +119,10 @@ export default {
         "--None--": [0,0]
       },
     */
+      saveSuccess: true,
+      attackValid: true,
       inputErrors: [],
-      saveError: Boolean
+      errorMsg: "值必须为数字"
     }
   },
   props: {
